4a0dd1fd394040031454938c16f938d88a5375ce
BHoffy <brandon.hoffman360@yahoo.com>
2021-10-19 19:44:36 -0400

    hw6

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -2,12 +2,15 @@ const SomeApp = {
     data() {
       return {
       books: [],
-            }
+      selectedBook: null,
+      bookForm: {}
+        }
     },
     computed: {},
     methods: {
         fetchBooksData() {
-            fetch('/api/books/')
+            //console.log("Fetching offer data for ", s);
+            fetch('/api/books/index.php')
             .then( response => response.json() )
             .then( (responseJson) => {
                 console.log(responseJson);
@@ -16,12 +19,37 @@ const SomeApp = {
             .catch( (err) => {
                 console.error(err);
             })
+            .catch( (error) => {
+                console.error(error);
+            });
         },
-    },
-    created() {
-        this.fetchBooksData();
+        postNewBook(evt) {
+            //this.offerForm.studentId = this.selectedStudent.id;        
+            
+            console.log("Posting!", this.bookForm);
+    
+            fetch('api/books/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+                
+                // reset the form
+                this.bookForm = {};
+              });
+          }
+      },
+      created() {
+          this.fetchBooksData();
+      }
+    
     }
-
-}
-
-Vue.createApp(SomeApp).mount('#bookApp');
+    
+    Vue.createApp(SomeApp).mount('#bookApp');
